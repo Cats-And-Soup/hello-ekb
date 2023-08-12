e19ec3dc0a2ceffb84cbb30c7ca99078fd399122
Konstantine <Konstaphy@gmail.com>
2023-08-12 16:00:05 +0500

    feat: карта c плейсмарками

--- src/pages/list/list.tsx
+++ src/pages/list/list.tsx
@@ -1,14 +1,27 @@
 import { YMaps, Map, Placemark } from "react-yandex-maps";
+import { useState } from "react";
+import * as React from "react";
+import "./list.css";
 
 export const List = () => {
+  const [type, setType] = useState<"map" | "list">("map");
   return (
-    <div>
-      <MapComponent />
+    <div className={"map-wrapper"}>
+      <div className={"list-type-button-wrapper"}>
+        <button onClick={() => setType(type === "map" ? "list" : "map")}>
+          Показать {type === "map" ? "списком" : "картой"}
+        </button>
+      </div>
+      {type === "map" ? <MapComponent /> : <ListRepresentation />}
     </div>
   );
 };
 
-export const MapComponent = () => {
+export const ListRepresentation: React.FC = () => {
+  return <div>Список</div>;
+};
+
+export const MapComponent: React.FC = () => {
   return (
     <YMaps>
       <Map
